b6995d9150e7f79842cbe810b00281edecb265c1
Hazel Alain <hazelo235@gmail.com>
2023-12-18 17:49:09 -0600

    v1

--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    @IBOutlet weak var text3: UITextField!
+    
+    var array = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        if let myArray = UserDefaults.standard.stringArray(forKey: "array"){
+            
+            text1.text = myArray[0]
+            text2.text = myArray[1]
+            text3.text = myArray[2]
+
+        }
     }
 
-
+    @IBAction func guardar(_ sender: UIButton) {
+        if text1.text = "" {
+            alerta(titulo: "Error", mensaje: "Completa todos los campos")
+        }
+        
+        else {
+            array.append(text1.text ?? "")
+            array.append(text2.text ?? "")
+            array.append(text3.text ?? "")
+            UserDefaults.standard.set(array, forKey: "array")
+            print("funciona")
+        }
+        
+    }
+    
+    
+    //alerta para usar
+    
+    func alerta(titulo : String, mensaje : String){
+        let alert = UIAlertController(title: titulo, message: mensaje, preferredStyle: .alert)
+        
+        let ok = UIAlertAction(title: "ok", style: .default, handler: nil)
+        
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
 }
 
